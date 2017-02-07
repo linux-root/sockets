d612fda05ddb87ac0454eb86948344fa59fb6461
Kurro <superusr.root@gmail.com>
2017-02-07 09:44:11 +0700

    add jquery and socket.io js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var PORT = process.env.PORT || 8888;
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 //create HTTP server
 //start mot server dung express lam mau
 // anything the Express listents to the http server should also listents to
-var http = require('http').Server(app);
+
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(){
+   console.log('User connected via socket.io');
+}); //listen to envents
 http.listen(PORT, function(){
   console.log('Server started'); // handle when start server
 });
